4833d5202fb47cac9413830e352c9a2fcb33ec6a
Zhidkov Gleb <zhidkob.gleb@gmail.com>
2018-06-25 23:55:31 +0300

    feature: remove lines buttons WIP. fix: now streams adds correct

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {distinctUntilChanged, switchMap} from 'rxjs/operators'
 import {streamFactory} from "./streamFactory";
 import {render} from "./view/render";
 import {getColor} from "./randomColorGenerator";
+import {of} from "rxjs/internal/observable/of";
 
 const addLine = document.querySelector("button");
 const getInterval = document.querySelector("input");
@@ -14,22 +15,29 @@ const draw$ = combineLatest()
         console.log(args)
     });
 
-const new$ = fromEvent(addLine, 'click')
-    .pipe(
-        switchMap(() => {
-            const pause = parseInt(getInterval.value) || 3000;
-            const color = getColor();
-            const newStream = streamFactory(pause, color);
-            lines.push(newStream);
-            return combineLatest(...lines)
-        })
-    );
-const removeButtons$ = new$.pipe(
-    distinctUntilChanged((streams) => streams.length)
+const new$ = fromEvent(addLine, 'click');
+
+const data$ = new$.pipe(
+    switchMap(() => {
+        const pause = parseInt(getInterval.value) || 3000;
+        const color = getColor();
+        const newStream = streamFactory(pause, color);
+        lines.push(newStream);
+        return combineLatest(...lines)
+    })
 );
-new$.subscribe((stream) => {
+
+data$.subscribe((stream) => {
     render(stream)
 });
+
+const removeButtons$ = new$.pipe(
+    distinctUntilChanged((streamsBef, streamsAfter) => {
+        console.log("fuck");
+        return streamsAfter.toString() === streamsBef.toString() + 'ff'
+    })
+);
+
 removeButtons$.subscribe((streams) => {
    console.log(streams)
 });
