1682151ed650a6d056d671fa9c61316d3c5bb13f
ZhidkovGV <zhidkov.gleb@gmail.com>
2018-06-25 17:58:53 +0300

    fix: scales done with few issues

--- src/view/countScales.ts
+++ src/view/countScales.ts
@@ -3,7 +3,7 @@ import {max, min} from "d3-array";
 const svg = document.querySelector('svg');
 
 export function countXScale(lines) {
-    const width = parseInt(getComputedStyle(svg).width) - 100;
+    const width = parseInt(getComputedStyle(svg).width) - parseInt(getComputedStyle(svg).paddingLeft);
     return scaleLinear().domain(
         [min(lines, (line) => min(line as Array<any>, (point) => point.seconds)),
             max(lines, (line) => max(line as Array<any>, (point) => point.seconds))] as Array<any>
@@ -11,7 +11,7 @@ export function countXScale(lines) {
 }
 
 export function countYScale(lines) {
-    const height = parseInt(getComputedStyle(svg).height) - 50;
+    const height = parseInt(getComputedStyle(svg).height);
     return scaleLinear().domain(
         [min(lines, (line) => min(line as Array<any>, (point) => point.val)),
             max(lines, (line) => max(line as Array<any>, (point) => point.val))] as Array<any>
--- src/view/renderAxises.ts
+++ src/view/renderAxises.ts
@@ -4,9 +4,9 @@ import {svg} from './render'
 
 export function renderXAxis(renderAxisData, scale) {
     const axis = axisBottom(scale).ticks(5);
-    svg.append("g").attr("class", "axis-bottom").attr("transform", "translate(0,100)").style('font-size', '2px').call(axis);
+    svg.append("g").attr("class", "axis-bottom").attr("transform", "translate(0,0)").call(axis);
 }
 export function renderYAxis (renderAxisData, scale) {
     const axis = axisLeft(scale).ticks(4);
-    svg.append("g").attr("class","axis-left" ).attr("transform", "translate(0, 0)").style('font-size', '2px').call(axis);
+    svg.append("g").attr("class","axis-left" ).attr("transform", "translate(0, 0)").call(axis);
 }
